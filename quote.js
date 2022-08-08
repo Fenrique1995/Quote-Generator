83cfea49d3c84a110cabb7bbe7e3fa69bffdac58
DESKTOP-M2JOLBR\Red Robin <fenrique310@gmail.com>
2022-08-08 18:01:21 -0300

    segundo intento

--- quote.js
+++ quote.js
@@ -4,11 +4,12 @@ const BOTON = document.querySelector("button");
 
 
 function frasesRandom() {
-    let url = fetch("http://api.quotable.io/random");
+    let url = fetch("https://api.quotable.io/random");
     url.then(res => res.json()).then(result => {
+        
         FRASE.innerText = result.content;
         AUTHOR.innerText = result.author;
     })
 }
-
+frasesRandom();
 BOTON.addEventListener("click", frasesRandom);
